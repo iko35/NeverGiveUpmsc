dbd36de6889b69bbcb8e594deef02fd7af3c8c02
K4RACA123 <yaguztuana41@gmail.com>
2024-08-15 20:39:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ SONG_DOWNLOAD_DURATION = int(
     getenv("SONG_DOWNLOAD_DURATION_LIMIT", "5400")
 )
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", 1002163836196))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002163836196))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6513314526))
