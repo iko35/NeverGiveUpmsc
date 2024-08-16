71ad6c5b26d2dc6663cb61f8bd7f5ae33febc914
K4RACA123 <yaguztuana41@gmail.com>
2024-08-16 10:05:48 +0300

    Update broadcast.py

--- AnonXMusic/plugins/misc/broadcast.py
+++ AnonXMusic/plugins/misc/broadcast.py
@@ -20,7 +20,7 @@ from config import adminlist
 IS_BROADCASTING = True
 
 
-@app.on_message(filters.command("broadcast", "reklam") & SUDOERS)
+@app.on_message(filters.command("reklam") & SUDOERS)
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
