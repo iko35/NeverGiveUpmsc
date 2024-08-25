402ebe344917d2beaca82d83f97c281a97c0ead1
K4RACA123 <yaguztuana41@gmail.com>
2024-08-25 15:16:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", 22967163))
 API_HASH = getenv("API_HASH", "454830b6b53539b54e69bda388c33dde")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "7028360071:AAEW5HtEzXeLHLZAthtF2NUOrWkyS7gNuTM")
+BOT_TOKEN = getenv("BOT_TOKEN", "7339683479:AAGzZLdafpxsS46mkCSaMfDeG1p6uNha6IY")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://mongoguess:guessmongo@cluster0.zcwklzz.mongodb.net/?retryWrites=true&w=majority")
